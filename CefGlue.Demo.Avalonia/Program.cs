cb97d443c04772d8226a981f82ad1c01609ca3d4
walterlv <xblero@gmail.com>
2024-07-01 14:59:13 +0800

    Make Avalonia designer work

--- CefGlue.Demo.Avalonia/Program.cs
+++ CefGlue.Demo.Avalonia/Program.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices;
 using Avalonia;
 using Xilium.CefGlue.Common;
 using Xilium.CefGlue.Common.Shared;
@@ -7,32 +6,30 @@ namespace Xilium.CefGlue.Demo.Avalonia
 {
     class Program
     {
-
         static int Main(string[] args)
         {
-            AppBuilder.Configure<App>()
-                      .UsePlatformDetect()
-                      .With(new Win32PlatformOptions()
-                      {
-                          // CompositionMode = new [] { Win32CompositionMode.WinUIComposition }
-                      })
-                      .AfterSetup(_ => CefRuntimeLoader.Initialize(new CefSettings() {
-#if WINDOWLESS
-                          WindowlessRenderingEnabled = true
-#else
-                          WindowlessRenderingEnabled = false
-#endif
-                      },
-                      customSchemes: new[] {
-                        new CustomScheme()
-                        {
-                            SchemeName = "test",
-                            SchemeHandlerFactory = new CustomSchemeHandler()
-                        }
-                      }))
-                      .StartWithClassicDesktopLifetime(args);
-                      
-            return 0;
+            return BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
         }
+
+        /// <summary>
+        /// Avalonia configuration, don't remove; also used by visual designer.
+        /// </summary>
+        /// <returns></returns>
+        public static AppBuilder BuildAvaloniaApp() =>
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .With(new Win32PlatformOptions()
+                {
+                    // CompositionMode = new [] { Win32CompositionMode.WinUIComposition }
+                })
+                .AfterSetup(_ => CefRuntimeLoader.Initialize(new CefSettings()
+                {
+#if WINDOWLESS
+                    WindowlessRenderingEnabled = true,
+#else
+                    WindowlessRenderingEnabled = false,
+#endif
+                }, customSchemes: new[] { new CustomScheme() { SchemeName = "test", SchemeHandlerFactory = new CustomSchemeHandler(), } }));
     }
 }
